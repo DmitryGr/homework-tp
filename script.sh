0e87e7f23375df87a11049407eed013eaf45af05
DmitryGr <mitia810@mail.ru>
2019-04-09 21:15:36 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -7,4 +7,4 @@ sudo apt-get install curl
 cmake "-DON_PI=1" "-DSYSROOT=../tools/arm-bcm2708/arm-bcm2708hardfp-linux-gnueabi/arm-bcm2708hardfp-linux-gnueabi/sysroot"
 make
 make install
-LD_LIBRARY_PATH=../lib ./usr/local/bin/Facade
+LD_LIBRARY_PATH=../usr/local/lib ./usr/local/bin/Facade
