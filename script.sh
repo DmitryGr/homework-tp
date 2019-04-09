2699941ce32891e023dca42d092a00b5dd8996b0
DmitryGr <mitia810@mail.ru>
2019-04-09 22:12:54 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,12 +3,12 @@
 sudo apt-get install libssl-dev
 sudo apt-get install curl
 
-compiler=../tools/arm-bcm2708/arm-linux-gnueabihf/bin
+cmp=$(pwd)/tools/arm-bcm2708/arm-linux-gnueabihf/bin
 
-PATH=$PATH:/$compiler
+PATH=$PATH:/$cmp
 
 
 cmake -DON_PI=ON -D SYSROOT=./tools/arm-bcm2708/arm-linux-gnueabihf/arm-linux-gnueabihf/sysroot .
 make
 sudo make install DESTDIR=../
-LD_LIBRARY_PATH=../usr/local/lib ./usr/local/bin/Facade
+LD_LIBRARY_PATH=./usr/local/lib ./usr/local/bin/Facade
